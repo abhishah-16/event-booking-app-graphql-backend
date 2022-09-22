67d5c05d8fc61b1a6506da1c1eb6bc0738b6556a
abhishah <abhishah0961@gmail.com>
2022-09-22 14:29:34 +0530

    setup: mongodb connection & create: Event schema

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql')
+const mongoose = require('mongoose')
+const Event = require('./models/event')
 const app = express()
 app.use(express.json())
 
@@ -35,18 +37,18 @@ app.use('/graphql', graphqlHTTP({
         }
     `),
     rootValue: {
-        events: () => {
+        events: async () => {
+            const events = await Event.find()
             return events
         },
-        createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+        createEvent: async (args) => {
+            const event = new Event({
                 title: args.input.title,
                 description: args.input.description,
-                price: +args.input.price,
-                date: new Date().toISOString()
-            }
-            events.push(event)
+                price: args.input.price,
+                date: new Date(args.input.date),
+            })
+            await event.save()
             return event
         }
     },
@@ -56,6 +58,11 @@ app.use('/graphql', graphqlHTTP({
 app.get('/', (req, res, next) => {
     res.send('GRAPHQL-APP')
 })
-app.listen(4000, () => {
-    console.log('SERVER IS RUNNING ON 4000');
+
+mongoose.connect('mongodb+srv://abhishah0196:abhishah@cluster0.w9drn.mongodb.net/event-booking-graphql', () => {
+    console.log('CONNECTED TO MONGODB ATLAS :)');
 })
+app.listen(4000, () => {
+    console.log('SERVER IS RUNNING ON 4000 :)');
+})
+// mongodb+srv://abhishah0196:<password>@cluster0.w9drn.mongodb.net/test
